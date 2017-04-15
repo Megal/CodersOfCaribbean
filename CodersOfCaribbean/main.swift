46b4531a5ca6408f9b5fc7d52819f4b67d6650bf
Svyatoshenko "Megal" Misha <m1sha.odesk@gmail.com>
2017-04-15 10:07:33 +0300

    Initial Commit

---
+++ CodersOfCaribbean/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodersOfCaribbean
+//
+//  Created by Svyatoshenko "Megal" Misha on 2017-04-15.
+//  Copyright © 2017 Megal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
